14b69e3df0c5cad5510cd750b9725d27314928d2
Kesia Mary Joies <kesia@strollby.com>
2024-07-04 17:06:27 +0530

    feat: [Example1] get movies

---
+++ data/movies.py
@@ -0,0 +1,34 @@
+# SOURCE: https://www.moviecrow.com/malayalam/Best-Movies-By-Year/2024
+
+movies = {
+  "Aadujeevitham": {
+    "name": "Aadujeevitham",
+    "genre": "DRAMA",
+    "release_date": "2024-03-28",
+    "rating": 70
+  },
+  "Anweshippin Kandethum": {
+    "name": "Anweshippin Kandethum",
+    "genre": "ACTION",
+    "release_date": "2024-02-09",
+    "rating": 68
+  },
+  "Premalu": {
+    "name": "Premalu",
+    "genre": "DRAMA",
+    "release_date": "2024-02-09",
+    "rating": 68
+  },
+  "Manjummel boys": {
+    "name": "Manjummel Boys",
+    "genre": "THRILLER",
+    "release_date": "2024-02-22",
+    "rating": 68
+  },
+  "Aavesham": {
+    "name": "Aavesham",
+    "genre": "ACTION",
+    "release_date": "2024-04-11",
+    "rating": 68
+  }
+}
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from data.movies import movies
+
 app = FastAPI()
 
 
@@ -11,3 +13,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/movies")
+async def get_movies():
+    return list(movies.values())
+
+
+
